54a43f74f971ee17e66e7db5d2a1056b88fceb9d
Emanuel <emanuellima4321@gmail.com>
2024-09-12 20:34:17 -0300

    Implementação: Scopus-Base

--- Banco-Brasil-Front/src/router/index.js
+++ Banco-Brasil-Front/src/router/index.js
@@ -1,23 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import { createRouter, createWebHistory } from 'vue-router'; 
+import Home from '@/Pages/Home.vue';
+import Simul from '@/Pages/Simul.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Home, 
+  },
+  {
+    path: '/Simul',
+    component: Simul, 
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(), 
+  routes,
+});
 
-export default router
+export default router;
--- README.md
+++
@@ -1,1 +0,0 @@
-# Banco-do-Brasil-Front-End-
